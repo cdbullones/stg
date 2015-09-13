b1160bb5bebbcb81b3f9156ff6e3542368d6dbe1
cdbullones <cdbullones@gmail.com>
2015-09-12 20:18:26 -0430

    commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="jumbotron">
+    				<h1>STG</h1>
+    					<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    					<p><a class="btn btn-primary btn-lg">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
